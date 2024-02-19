29a60c02acc0081e45a71960824162e10c1abcc9
Samarsinh999 <samarsinhgundpatil@gmail.com>
2024-02-19 16:48:17 +0530

    Self Implementation

--- app/src/main/java/com/example/colordetection/MainActivity.kt
+++ app/src/main/java/com/example/colordetection/MainActivity.kt
@@ -1,68 +1,33 @@
 package com.example.colordetection
 
 import android.os.Bundle
-import android.util.Log
 import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.example.colordetection.ui.theme.ColorDetectionTheme
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-
-
-@Composable
-fun SampleView() {
-    val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
-
-
-    val cameraController = LifecycleCameraController(context).apply {
-        bindToLifecycle(lifecycleOwner)
-        cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-    }
-    val previewView: PreviewView = PreviewView(context).apply {
-        controller = cameraController
-    }
-
-    val preview = Preview.Builder().build()
-    val cameraProvider = ProcessCameraProvider.getInstance(context)
-//    val viewFinder: PreviewView = findViewById(R.id.previewView)
-
-// The use case is bound to an Android Lifecycle with the following code
-    val camera = cameraProvider.get().bindToLifecycle(lifecycleOwner, CameraSelector.DEFAULT_BACK_CAMERA, preview)
-
-// PreviewView creates a surface provider and is the recommended provider
-
-
-    AndroidView(factory = { _ ->
-        previewView
-    }) { view ->
-        preview.setSurfaceProvider(view.surfaceProvider)
-
-    }
-
-
-//    preview.setSurfaceProvider(viewFinder.getSurfaceProvider())
-}
 
 class MainActivity : ComponentActivity() {
     private lateinit var cameraExecutor: ExecutorService
@@ -71,10 +36,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ColorDetectionTheme {
-                SampleView()
-//                CameraPreview()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CameraPreview()
+                }
             }
         }
+
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
@@ -86,53 +56,96 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CameraPreview() {
+    var cameraProvider: ProcessCameraProvider? by remember { mutableStateOf(null) }
+    var preview: Preview? by remember { mutableStateOf(null) }
+    var colorHash by remember { mutableStateOf<String?>(null) }
+
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
-    val cameraProvider by remember {
-        mutableStateOf(ProcessCameraProvider.getInstance(context))
-    }
 
-    val preview: Preview by remember { mutableStateOf(Preview.Builder().build()) }
 
     DisposableEffect(context) {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
 
-        Log.d("dispose check ", "calling")
+        cameraProvider = cameraProviderFuture.get()
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        preview = Preview.Builder().build()
 
-        cameraProvider.get().bindToLifecycle(
-            lifecycleOwner,
-            CameraSelector.DEFAULT_BACK_CAMERA,
-            preview
-        )
-        onDispose {
-            cameraProvider.get().unbindAll()
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+
+        imageAnalysis.setAnalyzer(
+            ContextCompat.getMainExecutor(context)
+        ) { imageProxy ->
+            val averageColor = calculateAverageColor(imageProxy)
+            colorHash = colorToHex(averageColor)
+            imageProxy.close()
         }
+        cameraProvider?.bindToLifecycle(
+            lifecycleOwner,
+            cameraSelector,
+            preview,
+            imageAnalysis
+        )
 
+        onDispose {
+            cameraProvider?.unbindAll()
+        }
     }
 
+    AndroidView(
+        factory = { ctx ->
+           PreviewView(ctx).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                setImplementationMode(androidx.camera.view.PreviewView.ImplementationMode.COMPATIBLE)
+            }
+        },
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black) // Set a background color if needed
+    ) { previewView ->
+        // Set the preview surface on the PreviewView
+        preview?.setSurfaceProvider(previewView.surfaceProvider)
+    }
+}
 
-    AndroidView(factory = { _ ->
-        androidx.camera.view.PreviewView(context)
-    }) { view ->
-        preview.setSurfaceProvider(view.surfaceProvider)
+fun calculateAverageColor(image: ImageProxy): Color {
+    val buffer = image.planes[0].buffer
+    val pixelStride = image.planes[0].pixelStride
+    val rowStride = image.planes[0].rowStride
+    val rowPadding = rowStride - pixelStride * image.width
 
+    val pixels = IntArray(image.width * image.height)
 
+    var offset = 0
+    for (row in 0 until image.height) {
+        for (col in 0 until image.width) {
+            val pixel = (buffer.get(offset).toInt() and 0xFF shl 16) or
+                    (buffer.get(offset + 1).toInt() and 0xFF shl 8) or
+                    (buffer.get(offset + 2).toInt() and 0xFF) or
+                    (buffer.get(offset + 3).toInt() and 0xFF shl 24)
+            pixels[row * image.width + col] = pixel
+            offset += pixelStride
+        }
+        offset += rowPadding
     }
 
-    /*  AndroidView(
-          factory = { ctx ->
-              androidx.camera.view.PreviewView(ctx).apply {
-                  layoutParams = ViewGroup.LayoutParams(
-                      ViewGroup.LayoutParams.MATCH_PARENT,
-                      ViewGroup.LayoutParams.MATCH_PARENT
-                  )
-                  setImplementationMode(androidx.camera.view.PreviewView.ImplementationMode.COMPATIBLE)
-              }
-          },
-          modifier = Modifier
-              .fillMaxSize()
-              .background(Color.Black) // Set a background color if needed
-      ) { previewView ->
-          // Set the preview surface on the PreviewView
-      }*/
+    val red = pixels.map { Color(it).red }.average()
+    val green = pixels.map { Color(it).green }.average()
+    val blue = pixels.map { Color(it).blue }.average()
+
+    return Color(red.toFloat(), green.toFloat(), blue.toFloat())
+}
+
+fun colorToHex(color: Color): String {
+    val red = (color.red * 255).toInt().toString(16).padStart(2, '0')
+    val green = (color.green * 255).toInt().toString(16).padStart(2, '0')
+    val blue = (color.blue * 255).toInt().toString(16).padStart(2, '0')
+    return "$red$green$blue"
 }
 
